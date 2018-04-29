2ec0be645680fccab445f48ed3cf08dbda56805c
Muazzama Anwar <muazzamasheikh@gmail.com>
2018-04-29 18:56:45 +0500

    added API call for full imei status

--- app/api/v1/resources/__init__.py
+++ app/api/v1/resources/__init__.py
@@ -1,4 +1,4 @@
-import requests
+import requests, json
 from flask_restful import Resource, Api
 from app import app
 
@@ -6,15 +6,26 @@ api = Api(app, prefix='/api/v1')
 
 class BasicStatus(Resource):
     def get(self, imei):
-        json = requests.get('http://dirbs.pta.gov.pk/coreapi/api/v1/tac/{}'.format(imei))
-        print(json.json())
-        return json.json()
+        if len(str(imei))==15 and isinstance(imei, int):
+            tac = int(str(imei)[:8])
+            tac_response = requests.get('http://dirbs.pta.gov.pk/coreapi/api/v1/tac/{}'.format(tac)).json()
+            imei_response = requests.get('http://dirbs.pta.gov.pk/coreapi/api/v1/imei/{}?include_seen_with=0'.format(imei)).json()
+            basic_status = dict(tac_response, **imei_response)
+            return basic_status
+        else:
+            return json.dumps({"message":"Bad IMEI format"}), 400
 
 class FullStatus(Resource):
     def get(self, imei):
-        return "inside /api/v1/fullstatus/{}".format(imei)
+        if len(str(imei))==15 and isinstance(imei, int):
+            tac = int(str(imei)[:8])
+            tac_response = requests.get('http://dirbs.pta.gov.pk/coreapi/api/v1/tac/{}'.format(tac)).json()
+            imei_response = requests.get('http://dirbs.pta.gov.pk/coreapi/api/v1/imei/{}?include_seen_with=1'.format(imei)).json()
+            full_status = dict(tac_response, **imei_response)
+            return full_status
+        else:
+            return json.dumps({"message":"Bad IMEI format"}), 400
 
 
-# noinspection PyTypeChecker
-api.add_resource(BasicStatus, '/basicstatus/<int: imei>')
-api.add_resource(FullStatus, '/fullstatus/<int: imei>')
+api.add_resource(BasicStatus, '/basicstatus/<int:imei>')
+api.add_resource(FullStatus, '/fullstatus/<int:imei>')
