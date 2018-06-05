c2cd95d828dfd068d7268044bad3c52ec4505a06
muazzama <muazzama.anwar@yahoo.com>
2018-06-05 12:05:32 +0500

    pagination revised

--- app/api/v1/resources/admin.py
+++ app/api/v1/resources/admin.py
@@ -1,16 +1,16 @@
 import requests
 from webargs.flaskparser import use_kwargs
-from app import Root
+from app import Root, BaseUrl
 from ..assets.error_handling import *
-from ..requests.status_request import status_args
+from ..requests.status_request import full_status_args
 from .common import CommonResoures
 
 resource = CommonResoures()
 
 class FullStatus():
 
-    @use_kwargs(status_args)
-    def get(self, imei, seen_with):
+    @use_kwargs(full_status_args)
+    def get(self, imei, seen_with, start, limit):
         try:
             response = {}
             tac = imei[:8] # slice TAC from IMEI
@@ -28,15 +28,44 @@ class FullStatus():
                     response['operating_system'] = full_status['gsma']['operating_system']
                     response['radio_access_technology'] = full_status['gsma']['bands']
                     response['classification_state'] = full_status['classification_state']
-                    if seen_with==1:
-                        response['associated_msisdn'] = full_status.get('seen_with')
                     blocking_conditions = full_status['classification_state']['blocking_conditions']
                     complain_status = resource.get_complaince_status(blocking_conditions, full_status.get('seen_with')) # get compliance status
                     response = dict(response, **complain_status) if complain_status else response
-                    return Response(json.dumps(response), status=200, mimetype='application/json')
+                    if seen_with == 1:
+                        response['associated_msisdn'] = full_status.get('seen_with')
+                        response, status = self.paginated_list(response, start, limit, imei, seen_with, '{}/fullstatus'.format(BaseUrl))
+                    status = 200
+                    return Response(json.dumps(response), status=status, mimetype='application/json')
                 else:
                     return custom_response("IMEI not found", 404, 'application/json')
             else:
                 return custom_response("Bad TAC format", 400, 'application/json')
         except Exception as e:
             print(e)
+
+    def paginated_list(self, data, start, limit, imei, seen_with, url):
+        count = len(data['associated_msisdn'])
+        if (count < start):
+            return "Index out of bound.", 400
+        # make response
+        data['start'] = start
+        data['limit'] = limit
+        data['count'] = count
+        # make URLs
+        # make previous url
+        if start == 1:
+            data['previous'] = ''
+        else:
+            start_copy = max(1, start - limit)
+            limit_copy = max(1, start - 1)
+            data['previous'] = url + '?imei=%s&seen_with=%d&start=%d&limit=%d' % (imei, seen_with, start_copy, limit_copy)
+        # make next url
+        if start + limit > count:
+            data['next'] = ''
+        else:
+            start_copy = start + limit
+            data['next'] = url + '?imei=%s&seen_with=%d&start=%d&limit=%d' % (imei, seen_with, start_copy, limit)
+        # finally extract result according to bounds
+        data['associated_msisdn'] = data['associated_msisdn'][(start - 1):(start - 1 + limit) if (start - 1 + limit) <= count else count]
+
+        return data, 200
--- app/api/v1/resources/public.py
+++ app/api/v1/resources/public.py
@@ -4,21 +4,21 @@ from flask import Response
 from webargs.flaskparser import use_args
 from app import Root
 from ..assets.error_handling import *
-from ..requests.status_request import status_args
+from ..requests.status_request import basic_status_args
 from .common import CommonResoures
 
 resource = CommonResoures()
 
 class BasicStatus():
 
-    @use_args(status_args)
-    def get(self, args):
+    @use_args(basic_status_args)
+    def get(self, imei):
         try:
             response = {}
-            tac = args['imei'][:8] # slice TAC from IMEI
+            tac = imei[:8] # slice TAC from IMEI
             if tac.isdigit(): # TAC format validation
                 tac_response = requests.get('{}/coreapi/api/v1/tac/{}'.format(Root, tac)).json() # dirbs core tac api call
-                imei_response = requests.get('{}/coreapi/api/v1/imei/{}?include_seen_with=1'.format(Root, args['imei'])).json() # dirbs core imei api call
+                imei_response = requests.get('{}/coreapi/api/v1/imei/{}?include_seen_with=1'.format(Root, imei)).json() # dirbs core imei api call
                 basic_status = dict(tac_response, **imei_response) # join api response
                 if basic_status['gsma']: # TAC verification
                     response['imei'] = basic_status['imei_norm']
