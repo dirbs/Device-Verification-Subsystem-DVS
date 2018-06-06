406b43f19414c3f86bd2f29cb5e40cf1f34c6df7
muazzama <muazzama.anwar@yahoo.com>
2018-06-06 13:48:26 +0500

    config file updated

--- server_config.py
+++ server_config.py
@@ -21,25 +21,3 @@ class DevelopmentConfig(BaseConfig):
     DEBUG = True
     TESTING = True
     SECRET_KEY = 'DevSecretKey'
-    ALLOWED_EXT = ['tsv']
-    UPLOAD_FOLDER = 'api/v1/assets'
-    BASE_URL = '/api/v1'
-    HOST = '127.0.0.1'
-    PORT = 5000
-    DIRBSCORE = 'http://dirbs.pta.gov.pk'
-
-
-class GlobalConfig(BaseConfig):
-    """Global Configurations"""
-    Global = dict(
-    MinImeiLength = 14,
-    MaxImeiLength = 16,
-    MinImeiRange = "000000",
-    MaxImeiRange = "100",
-    HelpUrl = "not confirmed",
-    BlocDate = "to be decided",
-    MinFileContent = 1,
-    MaxFileContent = 1000000
-    )
-
-
